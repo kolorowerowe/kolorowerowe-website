fac6d7919e9518e086be038bce697dcf739649f1
kolorowerowe <34072563+kolorowerowe@users.noreply.github.com>
2021-05-18 14:53:07 +0200

    Removed default vue files

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <h1>I'm Dominik Kołodziej</h1>
+    <p>
+      I am a student filled with a passion for self-development. I live in Krakow, the city that still I want to explore.
+    </p>
+    <p>
+      I received my BSc degree in  Computer Science at AGH University of Science and Technology in 2021. While studying I have actively participated in the student organization BEST AGH Kraków and organized events for students.
+    </p>
+    <p>
+      Programming is not only my field of study and source of income- I would like to make the world a little more interesting with the help of modern technologies.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutMe',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
